69b3bd163ee56a1baff9443f974d84f78ea88d5f
Renate Logina <r.logina@accenture.com>
2020-10-08 22:44:51 +0300

    Edited the neighbour counter, functional pause and resume added

--- ConsoleApp1/GameLogic.cs
+++ ConsoleApp1/GameLogic.cs
@@ -3,38 +3,35 @@ namespace GameofLife
 {
     using System;
     using System.Reflection.PortableExecutable;
+    using System.Security.Cryptography.X509Certificates;
     using System.Threading;
 
     public class GameLogic
     {
         //public int MyProperty { get; set; }
-        public int neighbours;
-        //public int CellCount;
-        Random random = new Random();
-        //pointerposition when printing out the array
-        public int xPos;
-        public int yPos;
+        private int _neighbours;
         //board size
-        public int SizeX;
-        public int SizeY;
+        private int _sizeX;
+        private int _sizeY;
         //amount of live cells at given iteration
         public int LiveCells;
         //stores and resets Generation array values
-        static string[,] Generation;
+        private string[,] Generation;
 
         //Sets the very first iteration of the game, randomly
         public void SetSeed(int boardSize)
         {
             //set a 2D array size, based on board size
-            SizeX = 50 * boardSize - 3;
-            SizeY = 14 + boardSize * boardSize;
-            Generation = new string[SizeY, SizeX];
+            _sizeX = 50 * boardSize - 3;
+            _sizeY = 14 + boardSize * boardSize;
+            Random random = new Random();
+            Generation = new string[_sizeY, _sizeX];
 
             //populates the first generation with 2 characters, chosen randomly
             string[] RandomFiller = new string[] { "\u25CF", " " };
-            for (int RowIndex = 0; RowIndex < SizeY; RowIndex++)
+            for (int RowIndex = 0; RowIndex < _sizeY; RowIndex++)
             {
-                for (int ColIndex = 0; ColIndex < SizeX; ColIndex++)
+                for (int ColIndex = 0; ColIndex < _sizeX; ColIndex++)
                 {
                     int CharacterIndex = random.Next(RandomFiller.Length);
                     Generation[RowIndex, ColIndex] = RandomFiller[CharacterIndex];
@@ -46,12 +43,12 @@ namespace GameofLife
         public void PrintArray()
         {
             //Sets starting point for printing, according to graphic location
-            xPos = 2;
-            yPos = 6;
-            for (int RowIndex = 0; RowIndex < SizeY; RowIndex++)//prints row
+            int xPos = 2;
+            int yPos = 6;
+            for (int RowIndex = 0; RowIndex < _sizeY; RowIndex++)//prints row
             {
                 Console.SetCursorPosition(xPos, yPos);
-                for (int ColIndex = 0; ColIndex < SizeX; ColIndex++)//prints individual cells in row (width)
+                for (int ColIndex = 0; ColIndex < _sizeX; ColIndex++)//prints individual cells in row (width)
                 {
                     //prints a single character
                     Console.Write(Generation[RowIndex, ColIndex]);
@@ -69,21 +66,21 @@ namespace GameofLife
         //Populates NewGen array with cells according to Generation cell positions and resets Generation array
         public void NewGeneration()
         {
-            string[,] NewGen = new string[SizeY, SizeX];
-            //Sifts through each cell, checking it's neighbours
-            for (int RowIndex = 0; RowIndex < SizeY; RowIndex++)
+            string[,] NewGen = new string[_sizeY, _sizeX];
+            //Sifts through each cell, checking it's _neighbours
+            for (int RowIndex = 0; RowIndex < _sizeY; RowIndex++)
             {
-                for (int ColIndex = 0; ColIndex < SizeX; ColIndex++)
+                for (int ColIndex = 0; ColIndex < _sizeX; ColIndex++)
                 {
-                    //Checks for neighbours of the cell
+                    //Checks for _neighbours of the cell
                     NeigbourCounter(RowIndex, ColIndex);
                     Console.SetCursorPosition(1, 28);
                     
                     //checks if cell is alive
                     if (Generation[RowIndex, ColIndex] == "\u25CF")
                     {
-                        //if has less than 2 or more than 3 live neighbours, it dies
-                        if (neighbours < 2 || neighbours > 3)
+                        //if has less than 2 or more than 3 live _neighbours, it dies
+                        if (_neighbours < 2 || _neighbours > 3)
                         {
                             NewGen[RowIndex, ColIndex] = " ";
                         }
@@ -96,7 +93,7 @@ namespace GameofLife
                     //if the cell is dead
                     else
                     {
-                        if (neighbours == 3)
+                        if (_neighbours == 3)
                         {
                             NewGen[RowIndex, ColIndex] = "\u25CF";
                         }
@@ -112,52 +109,75 @@ namespace GameofLife
             Generation = NewGen;
         }
 
-        //algorythm to check the neighbours of a particular cell
-        public void NeigbourCounter(int RowIndex, int ColIndex)
+        //algorythm to check the _neighbours of a particular cell
+        private void NeigbourCounter(int RowIndex, int ColIndex)
         {
-            neighbours = 0;
-            if (RowIndex > 0 && ColIndex > 0 && Generation[RowIndex - 1, ColIndex - 1] == "\u25CF")
+            _neighbours = 0;
+
+            //loops through a 3x3 square
+            for (int Row = -1; Row < 2; Row++)
             {
-                neighbours++;
+                for (int Col = - 1; Col < 2; Col++)
+                {
+                    if (RowIndex + Row > -1 && ColIndex + Col > -1 && Row + RowIndex < _sizeY && ColIndex + Col < _sizeX && Generation[RowIndex + Row, ColIndex + Col] == "\u25CF")
+                    {
+                        if (Row == 0 && Col == 0)//unfortunately faulty when using != WHYYY
+                        {
+                           //do nothing
+                        }
+                        else
+                        {
+                            _neighbours++;
+                        }
+                    }
+
+                }
             }
-            
-            if (RowIndex > 0 && Generation[RowIndex - 1, ColIndex] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (RowIndex > 0 && ColIndex < SizeX - 1 && Generation[RowIndex - 1, ColIndex + 1] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (ColIndex > 0 && Generation[RowIndex, ColIndex - 1] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (ColIndex < SizeX - 1 && Generation[RowIndex, ColIndex + 1] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (RowIndex < SizeY - 1 && ColIndex > 0 && Generation[RowIndex + 1, ColIndex - 1] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (RowIndex < SizeY - 1 && Generation[RowIndex + 1, ColIndex] == "\u25CF")
-            {
-                neighbours++;
-            }
-            if (RowIndex < SizeY - 1 && ColIndex < SizeX - 1 && Generation[RowIndex + 1, ColIndex + 1] == "\u25CF")
-            {
-                neighbours++;
-            }
+            //Check _neighbours for each cell during testing
+            //Console.SetCursorPosition(0, 0);
+            //Console.WriteLine("Cell nr {0},{1} has {2} _neighbours.", RowIndex, ColIndex, _neighbours);
+
+            //if (RowIndex > 0 && ColIndex > 0 && Generation[RowIndex - 1, ColIndex - 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}            
+            //if (RowIndex > 0 && Generation[RowIndex - 1, ColIndex] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (RowIndex > 0 && ColIndex < _sizeX - 1 && Generation[RowIndex - 1, ColIndex + 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (ColIndex > 0 && Generation[RowIndex, ColIndex - 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (ColIndex < _sizeX - 1 && Generation[RowIndex, ColIndex + 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (RowIndex < _sizeY - 1 && ColIndex > 0 && Generation[RowIndex + 1, ColIndex - 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (RowIndex < _sizeY - 1 && Generation[RowIndex + 1, ColIndex] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
+            //if (RowIndex < _sizeY - 1 && ColIndex < _sizeX - 1 && Generation[RowIndex + 1, ColIndex + 1] == "\u25CF")
+            //{
+            //    _neighbours++;
+            //}
         }
 
         //Counts cells in the Generation array
         public void CellCounter()
         {
             LiveCells = 0;
-            for (int RowIndex = 0; RowIndex < SizeY; RowIndex++)
+            for (int RowIndex = 0; RowIndex < _sizeY; RowIndex++)
             {
-                for (int ColIndex = 0; ColIndex < SizeX; ColIndex++)
+                for (int ColIndex = 0; ColIndex < _sizeX; ColIndex++)
                 {
                     if(Generation[RowIndex,ColIndex] == "\u25CF")
                     {
--- ConsoleApp1/GameUI.cs
+++ ConsoleApp1/GameUI.cs
@@ -3,13 +3,9 @@
     using System;
     using System.Threading;
     public class GameUI
-    {
-        //UserAction should store the string entered when navigating menu
-        public string UserAction = "";
-        GameLogic logic = new GameLogic();
+    { 
         //BoardSize stores the level of BoardSizeiculty(board BoardSize) entered buy user
-        public int BoardSize;
-
+        private int _boardSize;
         public void GameMenu()
         {
             Console.SetWindowSize(155, 30);
@@ -23,14 +19,14 @@
                               "\n\n\n                                                  ENTER Q TO QUIT");
 
             Console.SetCursorPosition(59, 22);
-            UserAction = Console.ReadLine().ToLower();
+            string UserAction = Console.ReadLine().ToLower();
             switch (UserAction)
             {
                 case "1":
                 case "2":
                 case "3":
                     Console.Clear();
-                    BoardSize = Convert.ToInt32(UserAction);
+                    _boardSize = Convert.ToInt32(UserAction);
                     GameBoard();
                     break;
                 default:
@@ -41,75 +37,71 @@
             }
         }
         //prints gameboard borders and any UI information of the particular game
-        public void GameBoard()
+        private void GameBoard()
         {
+            GameLogic gameLogic = new GameLogic();
             Console.CursorVisible = false;
 
-            Console.WriteLine("\n  Live cells: {0}", logic.LiveCells); 
-            Console.WriteLine("  BoardSize is {0}", BoardSize);
+            Console.WriteLine("\n  Live cells: {0}", gameLogic.LiveCells); 
+            Console.WriteLine("  BoardSize is {0}", _boardSize);
             Console.SetCursorPosition(25, 1);
             Console.WriteLine("Press P to pause and resume game");
             Console.SetCursorPosition(25, 2);
             Console.WriteLine("Press S to save game");
 
             //draws game board || vertical lines
-            for (int height = 5; height < 20 + BoardSize * BoardSize; height++)
+            for (int height = 5; height < 20 + _boardSize * _boardSize; height++)
             {
                 Console.SetCursorPosition(1, height);
                 Console.Write("\u2588");
-                Console.SetCursorPosition(50 * BoardSize-1, height);
+                Console.SetCursorPosition(50 * _boardSize - 1, height);
                 Console.Write("\u2588");
             }
             //draws game board --- horizontal lines
-            for (int width = 1; width < 50 * BoardSize; width++)
+            for (int width = 1; width < 50 * _boardSize; width++)
             {                
                 Console.SetCursorPosition(width, 5);
                 Console.Write("\u2584");
-                Console.SetCursorPosition(width, 20 + BoardSize * BoardSize);
+                Console.SetCursorPosition(width, 20 + _boardSize * _boardSize);
                 Console.Write("\u2580");
             }
 
             //reads and prints logic.
             int Iteration = 0;
-            logic.SetSeed(BoardSize);
+            gameLogic.SetSeed(_boardSize);
             //loops through iterations
             while(true)
             {
-                logic.PrintArray();
+                gameLogic.PrintArray();
                 Iteration++;
                 Console.SetCursorPosition(2, 3);
                 Console.WriteLine("Iteration NR: {0}", Iteration);
-                logic.NewGeneration();
+                gameLogic.NewGeneration();
                 Thread.Sleep(1000);
 
-                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.S)
+                //checks if user presses any key to pause or save
+                if (Console.KeyAvailable)
                 {
-                    Console.Clear();
-                    Console.SetCursorPosition(13, 5);
-                    Console.WriteLine("Save the game");
-                    break;
+                    if(Console.ReadKey(true).Key == ConsoleKey.S)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(13, 5);
+                        Console.WriteLine("Save the game");
+                        break;
+                    }
+                    else
+                    {
+                        if (Console.ReadKey(true).Key == ConsoleKey.S)
+                        {
+                            Console.Clear();
+                            Console.SetCursorPosition(13, 5);
+                            Console.WriteLine("Save the game");
+                            break;
+                        }
+                    }                   
                 }
-                else if (Console.KeyAvailable && Console.ReadKey(true).Key != ConsoleKey.S)
-                {
-                    //You can pause and resume at this point. The following line resumes the loop
-                    Console.ReadKey(true);
-                }
-
             }
             Console.ReadKey();
-
-            //stops the script
-            //while (!(Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.P))
-            //{
-            //    logic.PrintArray();
-            //    Iteration++;
-            //    Console.SetCursorPosition(2, 3);
-            //    Console.WriteLine("Iteration NR: {0}", Iteration);
-            //    logic.NewGeneration();
-            //    Thread.Sleep(1000);
-            //}
-
-
         }
 
     }
